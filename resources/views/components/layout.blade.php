c2e4ad526a686870839fce85abdc5201efed13a8
Alberto Paroni <albertoparoni@Mac.homenet.telecomitalia.it>
2026-03-15 14:24:33 +0100

    Scaffolding progetto Laravel con Fortify

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="it">
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>{{ $title ?? 'Fortify Autentication' }}</title>
+		<link href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital@0;1&display=swap" rel="stylesheet">
+		<link rel="icon" href="{{ asset('favicon.ico') }}" type="image/x-icon">
+		@vite('resources/css/app.css')
+	</head>
+	<body>
+		<nav class="navbar navbar-expand-lg bg-body-tertiary">
+			<div class="container-fluid">
+				<a class="navbar-brand" href="{{ route('home') }}">Fortify Autentication</a>
+				<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+					<span class="navbar-toggler-icon"></span>
+    			</button>
+    			<div class="collapse navbar-collapse" id="navbarNav">
+    				<ul class="navbar-nav">
+        				<li class="nav-item">
+            				<a class="nav-link {{ request()->routeIs('home') ? 'active' : '' }}" {{ request()->routeIs('home') ? 'aria-current=page' : '' }} href="{{ route('home') }}">Home</a>
+						</li>
+					</ul>
+				</div>
+  			</div>
+		</nav>
+		<main>
+			<div class="container">
+				{{ $slot }}
+			</div>
+		</main>
+		@vite('resources/js/app.js')
+	</body>
+</html>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,8 @@
+
+<div class="row">
+		<div class="col bg-danger">
+		<x-layout title="Home">
+			<h1>HOME</h1>	
+		</x-layout>
+	</div>
+</div>
